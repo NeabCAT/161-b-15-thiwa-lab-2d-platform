90004204e506763628fe0d96376ebf2f37f17126
NeabCAT <neabkub01@gmail.com>
2025-10-21 15:57:10 +0700

    AntCanWalk

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -1,25 +1,39 @@
+using System.Collections;
+using System.Threading;
 using UnityEngine;
 
 public class Ant : Enemy
 {
 
     [SerializeField] Vector2 velocity;
-    public Transform[] MovePoint; 
+    public Transform[] MovePoint;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.intialize(20);
+
+        DamageHit = 20;
+
+        //set speed and direction of movement
+        velocity = new Vector2(-1.0f, 0.0f);
     }
 
     public override void Behavior()
     {
-        throw new System.NotImplementedException();
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+
+    private void FixedUpdate() 
+    {
+        Behavior();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
 }
